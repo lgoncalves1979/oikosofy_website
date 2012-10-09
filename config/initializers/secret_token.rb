8a78842e0e86779355c6a7ae4296dedab56128f5
lgoncalves1979 <l.m.s.goncalves@gmail.com>
2012-10-09 22:11:16 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oikosofysite::Application.config.secret_token = 'b13f3eef6a5ed7cb25a9c6aa029f3292f9eb21b518425720bc0c5ca7f9854c6f289ad86d3e5985d9ec2250c7821807315079dd7c3e08d5e8692bb6375db2aaf2'
